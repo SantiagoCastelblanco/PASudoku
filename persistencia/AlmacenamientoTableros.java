a33c8be5191e47b1173e46fc60583fe1317b5990
Santiago Esteban Castelblanco Castiblanco <62969849+Absdag@users.noreply.github.com>
2022-09-14 17:09:53 +0000

    Archivos

--- persistencia/AlmacenamientoTableros.java
+++ persistencia/AlmacenamientoTableros.java
@@ -4,35 +4,26 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 public class AlmacenamientoTableros{
-
-  private int matriz2[][];
-  
-  public AlmacenamientoTableros(){
-    matriz2[9][9]=0;//matriz auxiliar
-    int fila=0;
+    public void LeerMatriz(String nombreArchivo){
     try{
-      //Hacemos lectura del txt
-    BufferedReader matriz =newBufferedReader(new FileReader ("Tablero1.txt"));
-      String linea = matriz.readLine();
-    while (linea != null){
-      //separo los caracteres, con los espacios que dejamos en txt
-      String [] enteros = linea.split("");
-      for ( int i = 0; i< enteros.length; i++){
-        matriz2[fila][i] = Integer.parseint(enteros[i]);
-        fila ++;
-      }Tablero1.close();
-        
-      
-    }
-      for (int i = 0; i < longitud; i++) {
-				for (int j = 0; j < longitud; j++)
-					System.out.print(matriz2[i][j] + " ");
-				System.out.println();
-			}
-      }
-    catch(FileNotFoundException e){
-      System.out.printIn("No se encontro el archivo");
-    }
-  }
- 
+        FileReader r = new FileReader(nombreArchivo);
+        BufferedReader buffer = new BufferedReader(r);
+        String temp = " ";
+        while (temp!=null){
+            temp = buffer.readLine();
+            String x;
+            x = temp;
+            String []arreglo = x.split(" ");
+        }if (temp == null){
+        System.out.println("Fallo");
+        }
+  
+                
+    }catch(Exception e){
+                System.out.println("No se encontro el archivo");
+    };
+
+    
+}
+}
 }
